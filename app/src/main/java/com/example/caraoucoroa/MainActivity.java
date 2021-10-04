6bdf67d457789c7e959c4c7bbe2586ddb2f4258d
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-10-04 18:26:04 -0300

    finalizando projetos

--- app/src/main/java/com/example/caraoucoroa/MainActivity.java
+++ app/src/main/java/com/example/caraoucoroa/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageJogar = findViewById(R.id.imageJogar);
+        imageJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),ResultadoActivity2.class);
+                // passar dados para proxima tela
+                int numero = new Random().nextInt(2);
+                intent.putExtra("numero", numero);
+
+
+                startActivity(intent);
+            }
+        });
     }
 }
